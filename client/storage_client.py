35a5b8c49a58bf8a3f7e73770e9b59459f148898
Luka <luka.akhalkatsi@makingscience.com>
2026-02-20 17:05:29 +0400

    feat: add upload and download

---
+++ client/storage_client.py
@@ -0,0 +1,21 @@
+from google.cloud import storage
+from utils.settings import settings
+import json
+
+class GCSClient:
+    def __init__(self):
+        self.client = storage.Client()
+        self.bucket = self.client.bucket(settings.ASTEROIDS_BUCKET)
+
+    def upload(self, df, destination):
+        blob = self.bucket.blob(destination)
+
+        csv_data = df.to_csv(index=False)
+
+        blob.upload_from_string(csv_data, content_type="text/csv")
+
+        print(f"Uploaded")
+
+    def download(self, source_blob_name):
+        blob = self.bucket.blob(source_blob_name)
+        return json.loads(blob.download_as_text())
